abf3ae529024622338f844eac85848803c33eba2
Sergio Gachuz <sergioegch@hotmail.com>
2025-06-12 23:31:40 -0600

    feat(AdminController): ajuste de controlador para solo tener métodos correspondientes

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 use Mockery\Generator\StringManipulation\Pass\Pass;
 
@@ -16,25 +17,6 @@ class AdminController extends Controller {
 
     public function index(){
         return view('admin.index');
-    }
-
-    public function users(User $user){
-        return view('admin.users', ['users' => $user->all()]);
-    }
-
-    public function createUser(){
-        return view('admin.users_create');
-    }
-
-    public function storeUser(Request $request){
-        $params = $request->validate([
-            'name' => ['required'],
-            'email' => ['required', 'email', 'unique:users,email'],
-            'password' => ['required', 'confirmed', Password::min(8)->letters()->numbers()->symbols()],
-        ]);
-
-        User::create($params);
-        return redirect('/adminonline/usuarios')->with('success', 'Usuario creado correctamente');
     }
 
 }
