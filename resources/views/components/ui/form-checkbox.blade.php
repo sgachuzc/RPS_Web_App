af0e9eb18b6270dd96e16827e8d15500d291b8ae
Sergio Gachuz <sergioegch@hotmail.com>
2025-06-15 20:52:37 -0600

    fix(attributes): Asignar atributo checked a input

--- resources/views/components/ui/form-checkbox.blade.php
+++ resources/views/components/ui/form-checkbox.blade.php
@@ -3,9 +3,13 @@
   'checked' => false
 ])
 
+@php
+  $isChecked = ($checked) ? 'checked' : '';
+@endphp
+
 <div class="container_form-checkbox">
   <label for="{{ $name }}">
     {{ $slot }}
   </label>
-  <input class="form-checkbox" type="checkbox" name="{{ $name }}" id="{{ $name }}" {{ ($checked) ? 'checked' : '' }} required>
+  <input class="form-checkbox" type="checkbox" name="{{ $name }}" id="{{ $name }}" {{ $isChecked }}>
 </div>
