5d35b8c0cafbb6d84a0670377a75f3c4ed9eb7b5
Sergio Gachuz <sergioegch@hotmail.com>
2025-06-21 13:40:47 -0600

    fix(Login): Cambio de correo por username para login

--- app/Http/Controllers/Admin/SessionController.php
+++ app/Http/Controllers/Admin/SessionController.php
@@ -11,7 +11,7 @@ class SessionController extends Controller {
     
     public function login(Request $request){
         $credentials = request()->validate([
-            "email" => ["required", "email"],
+            "username" => ["required"],
             "password" => ["required"],
         ]);
 
