febeaa980e1c35d196761230f10048e6e51ce78b
Sergio Gachuz <sergioegch@hotmail.com>
2025-07-10 22:24:29 -0600

    feat(Footer): maquetación de footer

---
+++ resources/views/components/ui/footer.blade.php
@@ -0,0 +1,26 @@
+<footer>
+  <div class="footer_container">
+    <div class="footer_info">
+      <div class="footer_logo" style="margin-bottom: 20px;">
+        <img src="{{ Vite::asset('resources/images/logo_white.png') }}" alt="Logo" width="100">
+      </div>
+      <p class="footer_text">Risk Reduction & Problem Solving Consulting Services</p>
+      <p class="footer_text">Copyright <span>Ⓒ</span> {{ date('Y') }}</p>
+      <p class="footer_text">Todos los derechos reservados</p>
+    </div>
+    <div class="footer_sprite">
+      <h3>Sigue nuestras redes sociales</h3>
+      <div class="footer_social">
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <img src="{{ Vite::asset('resources/images/logo_facebook.svg') }}" alt="Facebook" width="30">
+        </a>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <img src="{{ Vite::asset('resources/images/logo_instagram.svg') }}" alt="Instagram" width="30">
+        </a>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <img src="{{ Vite::asset('resources/images/logo_linkedin.svg') }}" alt="LinkedIn" width="30">
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
